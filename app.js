69a0f4dbcf40339ed254a3745ab8687fb6c9ae75
FFox3 <ffox211310@gmail.com>
2026-06-13 15:49:09 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,50 @@
-console.log("Faraway 起動");
+window.addEventListener("load", () => {
+
+const loadingText =
+document.getElementById("loadingText");
+
+const loadingScreen =
+document.getElementById("loadingScreen");
+
+const chatApp =
+document.getElementById("chatApp");
+
+const steps = [
+
+"通信船に連絡中...",
+
+"既読\n現在出港準備中...",
+
+"通信船が移動中...",
+
+"Farawayが到着しました"
+
+];
+
+let i = 0;
+
+const timer = setInterval(() => {
+
+i++;
+
+if(i < steps.length){
+
+loadingText.innerText = steps[i];
+
+}else{
+
+clearInterval(timer);
+
+setTimeout(() => {
+
+loadingScreen.style.display = "none";
+
+chatApp.style.display = "flex";
+
+},1000);
+
+}
+
+},2000);
+
+});
